8996515e963229d6d61fee9403c62d46ede6304a
sk15xm@brocku.ca <sk15xm@brocku.ca>
2022-02-09 12:52:16 +0800

    added json file + opens/reads it

---
+++ filereader.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+let data = fs.readFileSync("all.json");
+let parsedFile = JSON.parse(data);
+let found = false;
+parsedFile.forEach(element => {
+    console.log(element);
+    }
+);
